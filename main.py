31d4709e70c033f735b5ad9409e8317587e23de3
Luizmesquita43 <matheusnobrefe@gmail.com>
2024-12-07 19:54:10 -0300

    Calculadora

---
+++ main.py
@@ -0,0 +1,44 @@
+#
+#
+# class Conta:
+#     def __init__(self, titular):
+#         self.titular = titular
+#         self.saldo = 0
+#
+#     def sacar(self, qtd):
+#         if(qtd <= self.saldo):
+#             self.saldo -= qtd
+#         else:
+#             print('Saldo insuficiente')
+#
+#     def depositar(self, qtd):
+#         self.saldo += qtd
+#
+#     def consultar_saldo(self):
+#         print(self.saldo)
+#
+# minha_conta = Conta("Davi Noah")
+#
+# minha_conta.consultar_saldo()
+# minha_conta.sacar(200)
+#
+#
+# minha_conta.depositar(300)
+# minha_conta.sacar(200)
+# minha_conta.consultar_saldo()
+
+
+
+class Calculadora:
+
+    def __init__(self):
+        pass
+    def somar(selfself):
+        pass
+    def subtrair(self):
+        pass
+    def multiplicar(self):
+        pass
+    def dividir(self):
+        pass
+
